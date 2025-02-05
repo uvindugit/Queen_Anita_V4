915e1106f66f07533218989381263d2395d5f650
uvindugit <uvinduf249@gmail.com>
2025-02-06 01:13:27 +0530

    Update config.js

--- config.js
+++ config.js
@@ -4,14 +4,14 @@ global.owner = process.env.OWNER_NUMBER.split(',');
 global.anitav4 = process.env.BOT_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
 global.ownername = process.env.OWNER_NAME || "David Cyril";
 global.packname = process.env.PACK_NAME || "𝐐𝐔𝐄𝐄𝐍_𝐀𝐍𝐈𝐓𝐀-𝐕𝟒";
-global.author = process.env.AUTHOR || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
+global.author = process.env.AUTHOR || "🍂🖤𝗞𝗜𝗡𝗚 𝗔𝗡𝗝𝗔𝗡𝗔 𝗕𝗕𝗛 💦🥵🍂";
 global.channelname = process.env.CHANNEL_NAME || "𝐃𝐀𝐕𝐈𝐃 𝐂𝐘𝐑𝐈𝐋";
 global.channeljid = process.env.CHANNEL_JID || "120363315231436175@newsletter";
-global.autoTyping = process.env.AUTO_TYPING === 'true'; 
+global.autoTyping = process.env.AUTO_TYPING === 'false'; 
 global.autoRecord = process.env.AUTO_RECORD === 'true';
 global.autoViewStatus = process.env.AUTO_VIEW_STATUS === 'true';
 global.AUTOSTATUS_REACT = process.env.AUTOSTATUS_REACT === 'true';
-global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "💚";
+global.autoLikeEmoji = process.env.AUTO_LIKE_EMOJI || "🗿";
 
 global.LEVELUP = process.env.LEVELUP === 'true';
 
@@ -35,21 +35,21 @@ global.public = process.env.PUBLIC === 'true';
 global.ANTIDELETE = process.env.ANTIDELETE === 'true';
 global.unavailable = process.env.UNAVAILABLE === 'true';
 global.available = process.env.AVAILABLE === 'true';
-global.autoreadmessages = process.env.AUTO_READ_MESSAGES === 'true';
-global.chatbot = process.env.CHATBOT === 'true';
+global.autoreadmessages = process.env.AUTO_READ_MESSAGES === 'false';
+global.chatbot = process.env.CHATBOT === 'false';
 global.autoreact = process.env.AUTO_REACT === 'true';
 
-global.WELCOME = process.env.WELCOME === 'true';
+global.WELCOME = process.env.WELCOME === 'false';
 
-global.prefix = process.env.PREFIX || '.';
+global.prefix = process.env.PREFIX || ',';
 
-global.autobio = process.env.AUTO_BIO === 'true';
+global.autobio = process.env.AUTO_BIO === 'false';
 
 global.ANTICALL = process.env.ANTICALL === 'true';
 
-global.antilink = process.env.ANTILINK === 'true';
-global.antilinkkick = process.env.ANTILINK_KICK === 'true';
-global.antilinkwarn = process.env.ANTILINK_WARN === 'true';
+global.antilink = process.env.ANTILINK === 'false';
+global.antilinkkick = process.env.ANTILINK_KICK === 'false';
+global.antilinkwarn = process.env.ANTILINK_WARN === 'false';
 
 global.mess = {
     success: process.env.MESSAGE_SUCCESS || '𝙳𝚘𝚗𝚎',
